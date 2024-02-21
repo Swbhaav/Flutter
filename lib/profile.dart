e4e68f64a187576ce560abf887450ab904b5c6bc
Shovab <shovabk.c11@gmail.com>
2024-02-21 11:27:01 +0545

    Firebase

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,19 +1,36 @@
-
 import 'package:flutter/material.dart';
+import 'package:form/service/firebase_database_service.dart';
 
 class profilePage extends StatelessWidget {
-  const profilePage({super.key});
-
+  profilePage({super.key});
+  List<dynamic> usersList = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      title: Text('Profile'),
-        centerTitle: true,
-    ),
-          body: Center(
-        child: Text('This is Profile page'),
-    ),
-    );
+        appBar: AppBar(
+          title: Text('Profile'),
+          centerTitle: true,
+        ),
+        body: FutureBuilder(
+          future: FirebaseDatabaseService().getUsersInACollection(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error Fetching Users'),
+              );
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              usersList = snapshot.data as List;
+              return Center(
+                child: Text('${usersList[0]}'),
+              );
+            }
+            return Center(
+              child: SizedBox(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          },
+        ));
   }
 }
--- lib/service/firebase_database_service.dart
+++ lib/service/firebase_database_service.dart
@@ -3,19 +3,38 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirebaseDatabaseService{
   final dbInstance = FirebaseFirestore.instance;
+  final usersList= [];
 
-
+// this function is used to get a single user details from firebase
   Future getSingleUser()async{
     try{
-      CollectionReference userCollection = dbInstance.collection('users');
-      final userDoc = await userCollection.doc('user1').get();
+      CollectionReference usersCollection = dbInstance.collection('users');
+      final userDoc = await usersCollection.doc('user1').get();
       if(userDoc.exists){
         print('The use1 details is ${userDoc.data()}');
       }else{
         print('Data not found');
       }
     }catch (e){
+      print('Error fetching data $e');
 
     }
   }
+  // This function is used to ge list of user from a collection in firebase
+  Future getUsersInACollection() async{
+    try{
+      CollectionReference _usersCollection = await dbInstance.collection('users');
+      await _usersCollection.get().then((querySnapShot){
+        for (var doc in querySnapShot.docs){
+          usersList.add(doc.data());
+        }
+      });
+      print('Users list ${usersList.length}');
+      return usersList;
+
+    }catch(e){
+      print('Error while getting users $e');
+    }
+  }
 }
+
